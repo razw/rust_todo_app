7d2fa467081d328545d6e17de4260fa772ef6217
razw <12405976+razw@users.noreply.github.com>
2026-01-29 22:39:43 +0900

    log setting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use rust_todo_app::create_app;
+use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
+    // ログの初期化
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "rust_todo_app=debug,tower_http=debug".into()),
+        )
+        .init();
+
     let app = create_app("sqlite:todos.db").await;
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .expect("Failed to bind to address");
-    println!("Server running on http://127.0.0.1:3000");
+    
+    tracing::info!("Server running on http://127.0.0.1:3000");
+
     axum::serve(listener, app).await.expect("Server error");
 }
